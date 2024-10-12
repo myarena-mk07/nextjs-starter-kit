1cfdae3545e0f8e1d60ff5fccb4ae4f2adcc1922
myarena-mk07 <mitvaghani012@gmail.com>
2024-10-12 13:15:33 +0200

    google analytics added

--- app/dashboard/_components/dashbord-top-nav.tsx
+++ app/dashboard/_components/dashbord-top-nav.tsx
@@ -8,11 +8,6 @@ export default function DashboardTopNav() {
     <header className="flex h-14 items-center justify-between border-b px-4">
       <div className="flex items-center space-x-2">
         <Link href="/" className="font-semibold">
-          <img
-            src="/api/placeholder/32/32"
-            alt="Shot Beautifier Logo"
-            className="w-8 h-8"
-          />
           <span className="text-lg font-bold"> Shot Beautifier</span>
         </Link>
       </div>
